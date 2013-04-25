6c90267030c1d085caaa05fdee8741dec0f148d4
Alexander Brugh <abrugh@lanl.gov>
2013-04-25 15:04:46 -0600

    added downview, arrow keys select view (left, right, down)

--- wr-display.py
+++ wr-display.py
@@ -39,13 +39,14 @@ class View:
         self.newscale = numpy.linspace(0, sensor_w, width)
         self.height = height
         self.rawdata = data_init(sensor_w, sensor_h)
+        self.rawy = sensor_h - 1
         self.viewdata = data_init(width, height)
+        self.viewy = self.height - 1
         filler = num2gray(255)
         for x in xrange(self.width):
             for y in xrange(self.height):
                 self.viewdata[x,y] = filler
         self.hand = hand
-        self.y = self.height - 1
 
     def init_surface(self, x, y):
         self.surface = pygame.Surface((x, y), 0, 8)
@@ -64,25 +65,24 @@ class View:
         print "self.width=", self.width
         print "len(newscale)=", len(self.newscale)
         print "len(self.viewdata)=", len(self.viewdata)
-        print "self.y=", self.y
+        print "self.viewy=", self.viewy
+        print "self.rawy=", self.rawy
         print "self.viewdata.shape=", self.viewdata.shape
-        #for tmpy, line in enumerate(numpy.roll(self.rawdata.T, self.height-self.y-1, axis=0)[:self.height]):
         tmpi = 0
-        while tmpi < self.height:
-            #print tmpy
-            #self.viewdata[:,self.height-1-tmpy] = interp1d(sensor_linspace, line)(self.newscale)
-            self.viewdata[:,(self.y+tmpi)%self.height] = interp1d(sensor_linspace, self.rawdata.T[(self.y+tmpi)%self.height])(self.newscale)
-            tmpi += 1
-        self.y = (self.y+tmpi)%self.height
+        for tmpi in range(self.height):
+            self.viewdata[:,tmpi] = interp1d(sensor_linspace, self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
+        
+        self.viewy = 0
 
     def new_line(self, line):
         """ Puts the newest line of data in the array, updates the y coord """
+        if self.hand == 'left':
+            line = line[::-1]
         for x, point in enumerate(line):
-            self.rawdata[x,self.y] = num2gray(ord(point))
-        self.viewdata[:,self.y%self.height] = interp1d(sensor_linspace, self.rawdata[:,self.y])(self.newscale)
-        self.y = (self.y%self.height) - 1
-        if self.y == -1:
-            self.y = self.height - 1
+            self.rawdata[x, self.rawy] = num2gray(ord(point))
+        self.viewdata[:,self.viewy] = interp1d(sensor_linspace, self.rawdata[:,self.rawy])(self.newscale)
+        self.rawy  = (self.rawy  - 1) % sensor_h
+        self.viewy = (self.viewy - 1) % self.height
 
     def draw(self):
         """Handles the rendering of the surface for this view. Data
@@ -93,10 +93,45 @@ class View:
         # This gives us a constant scroll effect, new data always appears
         # at the top, shifting everything down
         pygame.surfarray.blit_array(self.surface,
-                         numpy.roll(self.viewdata, self.height-self.y-1, axis=1))
-        if self.hand == 'left':
-            pygame.transform.flip(self.surface, True, False)
+                 numpy.roll(self.viewdata, self.height-self.viewy-1, axis=1))
+
+
+class DownView(View):
+    def __init__(self, width=640, height=480):
+        View.__init__(self, width, height)
+        self.rawx = sensor_h
+        self.viewx = width - 1
+        self.newscale = numpy.linspace(0, sensor_h, height)
+
+    def resize(self, x, y):
+        self.init_surface(x, y)
+        self.width = x
+        self.newscale = numpy.linspace(0, sensor_w, x)
+        self.height = y
+        self.viewdata = data_init(x, y)
+
+        self.newscale = numpy.linspace(0, sensor_h, y)
+
+        self.rescale()
+
+    def rescale(self):
+        tmpi = 0
+        for tmpi in range(self.height):
+            self.viewdata[tmpi,:] = interp1d(sensor_linspace, self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
         
+        self.viewx = 0
+
+
+    def new_line(self, line):
+        for x, point in enumerate(line):
+            self.rawdata[x, self.rawy] = num2gray(ord(point))
+        self.viewdata[self.viewx,:] = interp1d(sensor_linspace, self.rawdata[:,self.rawy])(self.newscale)
+        self.rawy = (self.rawy - 1) % sensor_h
+        self.viewx = (self.viewx - 1) % self.width
+
+    def draw(self):
+        pygame.surfarray.blit_array(self.surface,
+                 numpy.roll(self.viewdata, self.width-self.viewx-1, axis=0))
 
 # printable character mapping for hex dumping
 FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
@@ -106,7 +141,7 @@ def dump2(src, length=16):
     result=[]
     for i in xrange(0, len(src), length):
        s = src[i:i+length]
-       hexa = ' '.join(['%02X'%ord(x) for x in s])
+       hexa = ' '.join(['%02X' % ord(x) for x in s])
        printable = s.translate(FILTER)
        result.append('%04X   %-*s   %s\n' % (i, length*3, hexa, printable))
     return ''.join(result)
@@ -124,7 +159,8 @@ def main(opts, args):
 
     packets = pcap.open(file(infile))
     sensed = 0
-    surfaces = {'left':View(hand='left'), 'right':View(), 'down':[]}
+    surfaces = {'left':View(hand='left'), 'right':View(), 'down':DownView()}
+    inview = surfaces['right']
     for bs, i in packets:
         pkts -= 1
         ipheader = i[:42]
@@ -149,54 +185,33 @@ def main(opts, args):
 
             #print dump2(i)
             data = i[4:]
-            if sensor in ['right', 'left']:
+            if sensor in ['right', 'left', 'down']:
                 surfaces[sensor].new_line(data)
-#            elif sensor == "down":
-#                surfaces[sensor].append(pygame.Surface((1,400)))
-#                for x, j in enumerate(data[4:h+4]):
-#                    surfaces[sensor][-1].set_at((0,x),((ord(j),ord(j),ord(j))))
         if sensed > 2:
             sensed = 0
-            surfaces['right'].draw()
-            screen.blit(surfaces["right"].surface, (0,0))
-            # Scaling data to the window is super expensive (for netbooks)
-            #pygame.transform.smoothscale(surfaces['right'].surface,
-            #            (screenw, screenh), screen)
-
-
-    #        for sensor in ["left", "right"]:
-    #            if sensor == "right":
-    #                offset = w/3
-    #            if len(surfaces[sensor]) > h:
-    #                surfaces[sensor].pop(0) 
-    #            for x,i in enumerate(surfaces[sensor][::-1]):
-    #                if sensor == "left":
-    #                    screen.blit(pygame.transform.flip(
-#                           pygame.transform.smoothscale(i, (w/3, 1)), 1, 0), (offset,x))
-    #                else:
-    #                    screen.blit(pygame.transform.smoothscale(i,(w/3,1)), (offset,x))
-    #        for sensor in ["down"]:
-    #            offset = 2*(w/3)
-    #            if len(surfaces[sensor]) > w/3:
-    #                surfaces[sensor].pop(0)
-    #            for x,i in enumerate(surfaces[sensor][::-1]):
-    #                screen.blit(i,(w-x,0))
-
+            inview.draw()
+            screen.blit(inview.surface, (0,0))
             clock.tick() 
             pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return
-    #            elif event.key == pygame.K_LEFT:
-    #            elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_LEFT:
+                    inview = surfaces['left']
+                elif event.key == pygame.K_RIGHT:
+                    inview = surfaces['right']
+                elif event.key == pygame.K_DOWN:
+                    inview = surfaces['down']
             elif event.type == pygame.VIDEORESIZE:
                 print "RESIZE"
                 screenw, screenh = event.dict['size']
                 screen = pygame.display.set_mode((screenw, screenh), pygame.RESIZABLE)
-                surfaces["right"].resize(screenw, screenh)
+                for surface in surfaces.values():
+                    surface.resize(screenw, screenh)
                 print screeninfo.current_w, screeninfo.current_h
 
         if pkts == 0:
