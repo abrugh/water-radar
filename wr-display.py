90d21067bfa6ced5b1459e7b709a4e34861696b8
Alexander Brugh <abrugh@lanl.gov>
2013-05-05 19:48:53 -0600

    added an 'all view' that's activated by the up key. cleaned up the code to get a pylint score > 7

--- wr-display.py
+++ wr-display.py
@@ -12,17 +12,19 @@ sensor_h = 1000 # This is all the more data we'll buffer
 
 # Initial window size hardcoded to something small/reasonable
 screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE, 24)
-screen.fill((255, 255, 255))
+screen.fill((0, 0, 0))
 clock = pygame.time.Clock()
 
-def num2gray(n):
+def num2gray(num):
     """Utility function for returning greyscale ints from numbers returned by
     ord when run against single characters 
     """
-    return sum([n << x for x in range(0, 24, 8)])
+    return sum([num << x for x in range(0, 24, 8)])
 
 def data_init(x, y):
+    """ Utility function for initalizing data areas """
     return numpy.zeros((x, y), dtype=numpy.int8)
+
 
 class View:
     """ A class to collect and render data from various sensors
@@ -41,13 +43,14 @@ class View:
         self.rawy = sensor_h - 1
         self.viewdata = data_init(width, height)
         self.viewy = self.height - 1
-        filler = num2gray(255)
+        filler = num2gray(0)
         for x in xrange(self.width):
             for y in xrange(self.height):
                 self.viewdata[x,y] = filler
         self.hand = hand
 
     def init_surface(self, x, y):
+        """ Creates a new surface and sets the palette """
         self.surface = pygame.Surface((x, y), 0, 8)
         self.surface.set_palette([(c, c, c) for c in range(0, 256)])
 
@@ -63,7 +66,8 @@ class View:
     def rescale(self):
         """Rescales all data lines onto newly resized surface"""
         for tmpi in range(self.height):
-            self.viewdata[:,tmpi] = interp1d(sensor_linspace, self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
+            self.viewdata[:,tmpi] = interp1d(sensor_linspace, 
+                       self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
         
         self.viewy = 0
 
@@ -73,7 +77,8 @@ class View:
             line = line[::-1]
         for x, point in enumerate(line):
             self.rawdata[x, self.rawy] = num2gray(ord(point))
-        self.viewdata[:,self.viewy] = interp1d(sensor_linspace, self.rawdata[:,self.rawy])(self.newscale)
+        self.viewdata[:,self.viewy] = interp1d(sensor_linspace,
+                                       self.rawdata[:,self.rawy])(self.newscale)
         self.rawy  = (self.rawy  - 1) % sensor_h
         self.viewy = (self.viewy - 1) % self.height
 
@@ -90,6 +95,10 @@ class View:
 
 
 class DownView(View):
+    """ Class to handle the down-only view, inherrits from the regular View
+    class but has to do things a little differently
+    """
+
     def __init__(self, width=640, height=480):
         View.__init__(self, width, height)
         self.rawx = sensor_h
@@ -97,6 +106,7 @@ class DownView(View):
         self.newscale = numpy.linspace(0, sensor_h, height)
 
     def resize(self, x, y):
+        """Resizes the surface and the scaling factor for that surface"""
         self.init_surface(x, y)
         self.width = x
         self.newscale = numpy.linspace(0, sensor_w, x)
@@ -108,35 +118,46 @@ class DownView(View):
         self.rescale()
 
     def rescale(self):
+        """Rescales all data lines onto newly resized surface"""
         tmpi = 0
-        for tmpi in range(self.height):
-            self.viewdata[tmpi,:] = interp1d(sensor_linspace, self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
+        for tmpi in range(self.width):
+            self.viewdata[tmpi,:] = interp1d(sensor_linspace, 
+                       self.rawdata.T[(self.rawy+tmpi)%sensor_h])(self.newscale)
         
         self.viewx = 0
 
 
     def new_line(self, line):
+        """ Puts the newest line of data in the array
+        updates the x and y coord
+        """
+
         for x, point in enumerate(line):
             self.rawdata[x, self.rawy] = num2gray(ord(point))
-        self.viewdata[self.viewx,:] = interp1d(sensor_linspace, self.rawdata[:,self.rawy])(self.newscale)
+        self.viewdata[self.viewx,:] = interp1d(sensor_linspace,
+                                       self.rawdata[:,self.rawy])(self.newscale)
         self.rawy = (self.rawy - 1) % sensor_h
         self.viewx = (self.viewx - 1) % self.width
 
     def draw(self):
+        """Handles the rendering of the surface for this view. Data
+        is scrolled once the buffer is filled by enough calls to new_line.
+        """
         pygame.surfarray.blit_array(self.surface,
                  numpy.roll(self.viewdata, self.width-self.viewx-1, axis=0))
 
+
 # printable character mapping for hex dumping
-FILTER =''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
+FILTER = ''.join([(len(repr(chr(i)))==3) and chr(i) or '.' for i in range(256)])
 
 def dump2(src, length=16):
     """Utility function to print hex dumps"""
-    result=[]
-    for i in xrange(0, len(src), length):
-        s = src[i:i+length]
+    result = []
+    for idx in xrange(0, len(src), length):
+        s = src[idx:idx+length]
         hexa = ' '.join(['%02X' % ord(x) for x in s])
         printable = s.translate(FILTER)
-        result.append('%04X   %-*s   %s\n' % (i, length*3, hexa, printable))
+        result.append('%04X   %-*s   %s\n' % (idx, length*3, hexa, printable))
     return ''.join(result)
 
 
@@ -153,13 +174,14 @@ def main(opts, args):
     packets = pcap.open(file(infile))
     sensed = 0
     surfaces = {'left':View(hand='left'), 'right':View(), 'down':DownView()}
-    inview = surfaces['right']
-    for bs, i in packets:
+    inview = 'right'
+    for _, pktdata in packets:
         pkts -= 1
-        ipheader = i[:42]
-        i = i[42:]
-        dataheader = i[:21]
-        i = i[21:]
+        # Skip the ip header
+        _ = pktdata[:42]
+        pktdata = pktdata[42:]
+        dataheader = pktdata[:21]
+        pktdata = pktdata[21:]
         #print dump2(dataheader)
         if not dataheader:
             continue
@@ -176,42 +198,72 @@ def main(opts, args):
                 sensor = 'right'
                 sensed += 1
 
-            data = i[4:]
-            if sensor in ['right', 'left', 'down']:
+            data = pktdata[4:]
+            if sensor:
                 surfaces[sensor].new_line(data)
             else:
-                print dump2(i)
+                print dump2(pktdata)
                 continue
         else:
             continue
 
         if sensed > 2:
             sensed = 0
-            inview.draw()
-            screen.blit(inview.surface, (0, 0))
+            if inview != 'all':
+                surfaces[inview].draw()
+                screen.blit(surfaces[inview].surface, (0, 0))
+            else:
+                offset = 0
+                for view in ['left', 'right', 'down']:
+                    surfaces[view].draw()
+                    screen.blit(surfaces[view].surface, (offset, 0))
+                    offset += screenw/3
+                    
             clock.tick() 
             pygame.display.update()
 
         for event in pygame.event.get():
+            setviewall = False
+            setviewone = False
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return
                 elif event.key == pygame.K_LEFT:
-                    inview = surfaces['left']
+                    if inview == 'all':
+                        setviewone = True
+                    inview = 'left'
                 elif event.key == pygame.K_RIGHT:
-                    inview = surfaces['right']
+                    if inview == 'all':
+                        setviewone = True
+                    inview = 'right'
                 elif event.key == pygame.K_DOWN:
-                    inview = surfaces['down']
+                    if inview == 'all':
+                        setviewone = True
+                    inview = 'down'
+                elif event.key == pygame.K_UP:
+                    setviewall = True
+                    if inview != 'all':
+                        inview = 'all'
             elif event.type == pygame.VIDEORESIZE:
                 print "RESIZE"
                 screenw, screenh = event.dict['size']
                 screen = pygame.display.set_mode((screenw, screenh),
                                                  pygame.RESIZABLE)
-                for surface in surfaces.values():
-                    surface.resize(screenw, screenh)
+                if inview == 'all':
+                    setviewall = True
+                else:
+                    setviewone = True
                 print screeninfo.current_w, screeninfo.current_h
+
+            if setviewall:
+                for view in ['left','right']:
+                    surfaces[view].resize(screenw/3, screenh)
+                surfaces['down'].resize(screenw/2, screenh)
+            if setviewone:
+                for view in ['left', 'right', 'down']:
+                    surfaces[view].resize(screenw, screenh)
 
         if pkts == 0:
             return
@@ -219,7 +271,7 @@ def main(opts, args):
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-c', '--count', default=-1, action='store', type='int',
-                        help='Packets to read from a pcap file before exiting')
+                      help='Packets to read from a pcap file before exiting')
 
     options, arguments = parser.parse_args(sys.argv)
     main(options, arguments)
